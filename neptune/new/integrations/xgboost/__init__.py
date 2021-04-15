c84c582b4382b94383faf7a8fc727b18f6fd5ad1
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-04-15 20:23:13 +0200

    added cv

--- neptune/new/integrations/xgboost/__init__.py
+++ neptune/new/integrations/xgboost/__init__.py
@@ -30,38 +30,71 @@ class NeptuneCallback(xgb.callback.TrainingCallback):
                  max_num_features=None,  # requires matplotlib
                  log_tree=False):  # requires graphviz
         self.run = run
-        self.base_namespace = base_namespace
+        if base_namespace is None:
+            self.base_namespace = 'training'
+        else:
+            self.base_namespace = base_namespace
         self.log_model = log_model
         self.log_importance = log_importance
         self.max_num_features = max_num_features
         self.log_tree = log_tree
+        self.cv = False
 
     def before_training(self, model):
+        if hasattr(model, 'cvfolds'):
+            self.cv = True
         return model
 
     def after_training(self, model):
-        self.run['/'.join([self.base_namespace, 'booster_config'])] = json.loads(model.save_config())
-        if 'best_score' in model.attributes().keys():
-            self.run['/'.join([self.base_namespace, 'best_score'])] = model.attributes()['best_score']
-        if 'best_iteration' in model.attributes().keys():
-            self.run['/'.join([self.base_namespace, 'best_iteration'])] = model.attributes()['best_iteration']
+        if self.cv:
+            for i, fold in enumerate(model.cvfolds):
+                self.run['/'.join([self.base_namespace, 'fold_{}'.format(i), 'booster_config'])]\
+                    = json.loads(fold.bst.save_config())
+        else:
+            self.run['/'.join([self.base_namespace, 'booster_config'])] = json.loads(model.save_config())
+            if 'best_score' in model.attributes().keys():
+                self.run['/'.join([self.base_namespace, 'best_score'])] = model.attributes()['best_score']
+            if 'best_iteration' in model.attributes().keys():
+                self.run['/'.join([self.base_namespace, 'best_iteration'])] = model.attributes()['best_iteration']
 
         if self.log_importance:
-            importance = xgb.plot_importance(model, max_num_features=self.max_num_features)
-            self.run['/'.join([self.base_namespace, 'plots', 'importance'])].upload(
-                neptune.types.File.as_image(importance.figure))
-            plt.close('all')
+            if self.cv:
+                for i, fold in enumerate(model.cvfolds):
+                    importance = xgb.plot_importance(fold.bst, max_num_features=self.max_num_features)
+                    self.run['/'.join([self.base_namespace, 'fold_{}'.format(i), 'plots', 'importance'])].upload(
+                        neptune.types.File.as_image(importance.figure))
+                plt.close('all')
+            else:
+                importance = xgb.plot_importance(model, max_num_features=self.max_num_features)
+                self.run['/'.join([self.base_namespace, 'plots', 'importance'])].upload(
+                    neptune.types.File.as_image(importance.figure))
+                plt.close('all')
 
         if self.log_tree is not None:
-            trees = []
-            for j in self.log_tree:
-                tree = xgb.plot_tree(model, num_trees=j)
-                trees.append(neptune.types.File.as_image(tree.figure))
-            self.run['/'.join([self.base_namespace, 'plots', 'trees'])] = neptune.types.FileSeries(trees)
-            plt.close('all')
+            if self.cv:
+                for i, fold in enumerate(model.cvfolds):
+                    trees = []
+                    for j in self.log_tree:
+                        tree = xgb.plot_tree(fold.bst, num_trees=j)
+                        trees.append(neptune.types.File.as_image(tree.figure))
+                    self.run['/'.join([self.base_namespace, 'fold_{}'.format(i), 'plots', 'trees'])]\
+                        = neptune.types.FileSeries(trees)
+                    plt.close('all')
+            else:
+                trees = []
+                for j in self.log_tree:
+                    tree = xgb.plot_tree(model, num_trees=j)
+                    trees.append(neptune.types.File.as_image(tree.figure))
+                self.run['/'.join([self.base_namespace, 'plots', 'trees'])] = neptune.types.FileSeries(trees)
+                plt.close('all')
 
         if self.log_model:
-            self.run['/'.join([self.base_namespace, 'model_pickle'])].upload(neptune.types.File.as_pickle(model))
+            if self.cv:
+                for i, fold in enumerate(model.cvfolds):
+                    self.run['/'.join([self.base_namespace, 'fold_{}'.format(i), 'model_pickle'])].upload(
+                        neptune.types.File.as_pickle(fold.bst))
+            else:
+                self.run['/'.join([self.base_namespace, 'model_pickle'])].upload(neptune.types.File.as_pickle(model))
         return model
 
     def before_iteration(self, model, epoch: int,
@@ -71,12 +104,21 @@ class NeptuneCallback(xgb.callback.TrainingCallback):
 
     def after_iteration(self, model, epoch: int,
                         evals_log: xgb.callback.CallbackContainer.EvalsLog) -> bool:
+        self.run['/'.join([self.base_namespace, 'epoch'])].log(epoch)
+
         for stage, metrics_dict in evals_log.items():
             for metric_name, metric_values in evals_log[stage].items():
-                if self.base_namespace is not None:
+                if self.cv:
+                    mean, std = metric_values[-1]
+                    self.run['/'.join([self.base_namespace, stage, metric_name, 'mean'])].log(mean)
+                    self.run['/'.join([self.base_namespace, stage, metric_name, 'std'])].log(std)
+                else:
                     self.run['/'.join([self.base_namespace, stage, metric_name])].log(metric_values[-1])
-        self.run['/'.join([self.base_namespace, 'epoch'])].log(epoch)
-        config = json.loads(model.save_config())
-        x = config['learner']['gradient_booster']['updater']['grow_colmaker']['train_param']['learning_rate']
-        self.run['/'.join([self.base_namespace, 'learning_rate'])].log(float(x))
+
+        if self.cv:
+            config = json.loads(model.cvfolds[0].bst.save_config())
+        else:
+            config = json.loads(model.save_config())
+        lr = config['learner']['gradient_booster']['updater']['grow_colmaker']['train_param']['learning_rate']
+        self.run['/'.join([self.base_namespace, 'learning_rate'])].log(float(lr))
         return False
